3e85a5b2287e693d18f8cd97e71fbfa86ebe4bd9
bachelore Kitchen <77671705+bachelorskitchen108@users.noreply.github.com>
2021-03-05 19:33:44 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+imgSlider = (anything) => {
+     document.querySelector(".pepsi").src = anything;
+};
+changeBgColor = (color) => {
+     const changeColor = document.querySelector(".main");
+     changeColor.style.background = color;
+};
